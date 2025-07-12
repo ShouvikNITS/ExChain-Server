193eeea5cdc914201f862a23152d319656a562b6
SubhaNITS150 <subhajyoti_ug_23@cse.nits.ac.in>
2025-07-12 10:25:08 +0530

    Added initial schema

---
+++ server.js
@@ -0,0 +1,28 @@
+import express from 'express';
+import dotenv from 'dotenv';
+// import {router} from "next/client.js";
+import cors from "cors"
+
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(express.json())
+app.use(cors());
+
+app.use(express.urlencoded({ extended: true }))
+
+app.get('/', (req, res) => {
+    try {
+        res.status(200).send('Hello World!');
+    } catch(e){
+        res.status(500).send({message : "Something went wrong"});
+    }
+})
+
+
+
+app.listen(port, () => {
+    console.log(`Server started on port http://localhost:${port}`);
+})
